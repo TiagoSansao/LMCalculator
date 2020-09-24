aa1db271eff8879ac0cb96d425b77921a3328b60
Tiago Sansao <tiagossansao@gmail.com>
2020-09-24 10:55:57 -0300

    Branch gh-pages added

--- src/Total.js
+++ src/Total.js
@@ -2,23 +2,25 @@ import React from 'react';
 import './Total.css';
 
 class Total extends React.Component {
-    saveTotal = () => {
+    constructor(props) {
+        super(props)
+        this.speedType = React.createRef();
+    }
+    saveTotal = (e) => {
+        e.preventDefault();
         let date = new Date();
-        let hours = date.getHours();
-        let minutes = date.getMinutes();
         let time = date.toLocaleDateString();
-        localStorage.setItem(localStorage.length + 1, `${time} ${hours}:${minutes} - ${this.props.total[0]} ${this.props.total[1]}`);
+        let jsonData = JSON.stringify([time, this.props.total, this.speedType.current.value]);
+        localStorage.setItem(localStorage.length + 1, jsonData);
         this.forceUpdate();
         return
     }
     getRegistrys = () => {
-        let registrys = [];
+        let registrys = []
         for (let i = 1; i <= localStorage.length; i++) {
-            registrys.push(localStorage.getItem(i));
+            registrys.push(JSON.parse(localStorage.getItem(i)));
         };
-        return registrys.map((registry) => (
-            <li className="registry" key={registry.toString()}> {registry.toString()} </li>
-        ))
+        return registrys.map((registry) => <tr key={registry}><td>{registry[0]}</td><td>{registry[1]}</td><td>{registry[2]}</td></tr>)
     }
     deleteRegistrys = () => {
         localStorage.clear();
@@ -29,7 +31,16 @@ class Total extends React.Component {
             return (
                 <section id="results">
                     <h2>Total</h2> <p>{this.props.total}</p>
-                    <input type="button" value="SALVAR" onClick={this.saveTotal} id="saveButton"/>
+                    <form>
+                        <select ref={this.speedType} className="speed-type">
+                            <option>Padrão</option>
+                            <option>Pesquisa</option>
+                            <option>Treino</option>
+                            <option>Fusão</option>
+                            <option>Cura</option>
+                        </select> <br/>
+                        <input type="submit" value="SALVAR" onClick={this.saveTotal} id="saveButton"/>
+                    </form>
                 </section>
                 )
         }
@@ -38,13 +49,29 @@ class Total extends React.Component {
                 <div>
                     <section id="results">
                         <h2>Total</h2> <p>{this.props.total}</p>
-                        <input type="button" value="SALVAR" onClick={this.saveTotal} id="saveButton"/>
+                        <form>
+                            <select ref={this.speedType} className="speed-type">
+                                <option>Padrão</option>
+                                <option>Pesquisa</option>
+                                <option>Treino</option>
+                                <option>Fusão</option>
+                                <option>Cura</option>
+                            </select> <br/>
+                            <input type="submit" value="SALVAR" onClick={this.saveTotal} id="saveButton"/>
+                        </form>
                     </section>
                     <section id="saved-registry">
-                        <h2>Registros</h2>
-                        <ol>
-                            {this.getRegistrys()}
-                        </ol>
+                        <table>
+                            <caption>REGISTROS</caption>
+                            <tbody>
+                                <tr>
+                                    <th>DATA</th>
+                                    <th>TEMPO</th>
+                                    <th>TIPO</th>
+                                </tr>
+                                {this.getRegistrys()}
+                            </tbody>
+                        </table>
                         <input type="button" value="DELETAR REGISTROS" onClick={this.deleteRegistrys} id="deleteButton"/>   
                     </section>
                 </div>
