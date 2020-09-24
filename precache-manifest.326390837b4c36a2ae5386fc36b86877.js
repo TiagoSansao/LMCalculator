efcacffd4716f0a4f139376d0f1f75d884fd23e9
Tiago Sansao <tiagossansao@gmail.com>
2020-09-24 10:51:31 -0300

    Updates

---
+++ precache-manifest.326390837b4c36a2ae5386fc36b86877.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9514a595c9cd6852eaad3028975b7f",
+    "url": "/LMCalculator/index.html"
+  },
+  {
+    "revision": "7ab42eec4194b1845924",
+    "url": "/LMCalculator/static/css/main.79cc538c.chunk.css"
+  },
+  {
+    "revision": "ab9021f499b960394232",
+    "url": "/LMCalculator/static/js/2.8a7c1563.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LMCalculator/static/js/2.8a7c1563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab42eec4194b1845924",
+    "url": "/LMCalculator/static/js/main.b3f7f80c.chunk.js"
+  },
+  {
+    "revision": "e16d4e9bc85b372a61ec",
+    "url": "/LMCalculator/static/js/runtime-main.06c3d0c1.js"
+  }
+]);
